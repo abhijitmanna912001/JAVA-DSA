07c040813bed42493caf26cb8e1fab57584fd6fb
Abhijit Manna <79353253+abhijitmanna912001@users.noreply.github.com>
2023-12-21 13:27:50 +0530

    SecondProgram

---
+++ SecondProgram.java
@@ -0,0 +1,13 @@
+public class SecondProgram {
+    public static void main(String[] args) {
+        int a = 25;
+        byte b = 25;
+        long x = 38;
+        short y = 789;
+        short z = (short) 32769;
+        System.out.println(z);
+
+        float f = 98.9F;
+        char ch = 'a';
+    }
+}
