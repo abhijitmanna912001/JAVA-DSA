c111e1f951456fe274deaec6e7e94330565ede97
Abhijit Manna <79353253+abhijitmanna912001@users.noreply.github.com>
2023-12-21 14:14:25 +0530

    Scanner class

---
+++ ThirdProgram.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class ThirdProgram {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("What is your name");
+
+        String name = sc.nextLine();
+
+        System.out.println("Hi " + name);
+        
+        sc.close();
+    }
+}
