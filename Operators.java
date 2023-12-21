941b649fa52e256708faf3bffdbba25bff6d3a90
Abhijit Manna <79353253+abhijitmanna912001@users.noreply.github.com>
2023-12-21 19:04:03 +0530

    Operators in Java

--- Operators.java
+++ Operators.java
@@ -7,5 +7,18 @@ public class Operators {
         int z = op1++;
         int a = op1--;
         System.out.println(op1 + " " + x + " " + y + " " + z + " " + a);
+
+        int p = op1++ + ++op1 - --op1 - op1++;
+        System.out.println(p);
+
+        boolean correct = true;
+        System.out.println(correct);
+        System.out.println(!correct);
+
+        int val1 = 10;
+        System.out.println(~val1);
+
+        int val2 = -10;
+        System.out.println(~val2);
     }
 }
