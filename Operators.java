925e2cbafefda7d27496fb9f26d1adc349a1bb38
Abhijit Manna <79353253+abhijitmanna912001@users.noreply.github.com>
2023-12-21 15:13:15 +0530

    Urany Operators

---
+++ Operators.java
@@ -0,0 +1,11 @@
+public class Operators {
+    public static void main(String[] args) {
+        int op1 = 10;
+        // op1--;
+        int y = --op1;
+        int x = ++op1;
+        int z = op1++;
+        int a = op1--;
+        System.out.println(op1 + " " + x + " " + y + " " + z + " " + a);
+    }
+}
