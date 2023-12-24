8ad8673db4fbe3ecc6b1e66ed2212dcaedc85371
Abhijit Manna <79353253+abhijitmanna912001@users.noreply.github.com>
2023-12-24 11:21:20 +0530

    Packages in Java

---
+++ Packages.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class Packages {
+    public static void main(java.lang.String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("hi");
+    }
+}
