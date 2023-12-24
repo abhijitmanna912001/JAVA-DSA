0bd5292eeea36ba7d0abfb00ac453ac079a99e4e
Abhijit Manna <79353253+abhijitmanna912001@users.noreply.github.com>
2023-12-24 22:57:01 +0530

    If-statement

---
+++ Ifelse.java
@@ -0,0 +1,8 @@
+public class Ifelse {
+    public static void main(String[] args) {
+        int marks = 89;
+
+        System.out.println("You are topper as you have secured " + marks);
+        System.out.println("Your marks is: " + marks);
+    }
+}
