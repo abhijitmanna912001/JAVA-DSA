0b93e35f9cd2bdac25bf55d7a62fc944c433654f
Abhijit Manna <79353253+abhijitmanna912001@users.noreply.github.com>
2023-12-25 22:55:52 +0530

    Switch Statements

---
+++ Switch.java
@@ -0,0 +1,19 @@
+public class Switch {
+    public static void main(String[] args) {
+        int choice = 4;
+
+        switch (choice) {
+            case 1:
+                System.out.println("Goa is on");
+                break;
+            case 2:
+                System.out.println("Pondicherry is on");
+                break;
+            case 3:
+                System.out.println("Manali is on");
+                break;
+            default:
+                System.out.println("Choose correctly");
+        }
+    }
+}
