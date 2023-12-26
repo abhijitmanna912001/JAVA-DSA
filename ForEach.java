b5d4ab584c7934663f68cf78e74c41ebe82e4580
Abhijit Manna <79353253+abhijitmanna912001@users.noreply.github.com>
2023-12-26 11:40:04 +0530

    ForEach loop

---
+++ ForEach.java
@@ -0,0 +1,8 @@
+public class ForEach {
+    public static void main(String[] args) {
+        int arr[] = { 5, 7, 90 };
+        for (int element : arr) {
+            System.out.println(element);
+        }
+    }
+}
