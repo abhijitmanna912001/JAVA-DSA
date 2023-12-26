d58b29123ffeae77dc757fd035d600fdc8f38897
Abhijit Manna <79353253+abhijitmanna912001@users.noreply.github.com>
2023-12-26 12:55:36 +0530

    While Loop

--- Loops.java
+++ Loops.java
@@ -1,12 +1,18 @@
 public class Loops {
     public static void main(String[] args) {
-        for (int num = 2; num < 4; num++) {
+        // for (int num = 2; num < 4; num++) {
 
-            for (int times = 1; times <= 10; times++) {
-                if (times == 5)
-                    continue;
-                System.out.println(num + "*" + times + ":\t" + num * times);
-            }
+        // for (int times = 1; times <= 10; times++) {
+        // if (times == 5)
+        // continue;
+        // System.out.println(num + "*" + times + ":\t" + num * times);
+        // }
+        // }
+
+        int number = 27;
+        while (number != 0) {
+            System.out.println(number);
+            number = number / 2;
         }
     }
 }
