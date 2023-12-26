15cb00f069b89d8d542d5be02ff496243adc02ad
Abhijit Manna <79353253+abhijitmanna912001@users.noreply.github.com>
2023-12-26 17:42:13 +0530

    Swap by Reference

--- Basic Java/Functions/MyFunc.java
+++ Basic Java/Functions/MyFunc.java
@@ -1,10 +1,22 @@
+class Temp {
+    int myVar = 10;
+}
+
 public class MyFunc {
     public static void main(String[] args) {
         MyFunc mf = new MyFunc();
 
-        int num1 = 30;
-        int num2 = 20;
-        mf.swap(num1, num2);
+        Temp obj1 = new Temp();
+        Temp obj2 = new Temp();
+
+        obj1.myVar = 10;
+        obj2.myVar = 20;
+        mf.swapByReference(obj1, obj2);
+        System.out.println("After call " + obj1.myVar + "," + obj2.myVar);
+
+        // int num1 = 30;
+        // int num2 = 20;
+        // mf.swap(num1, num2);
         // System.out.println(num1 + "," + num2);
         // int res = mf.calculateArea(5, 2);
         // System.out.println(res);
@@ -16,6 +28,14 @@ public class MyFunc {
         val1 = val2;
         val2 = temp;
         System.out.println(val1 + "," + val2);
+    }
+
+    public void swapByReference(Temp obj1, Temp obj2) {
+        System.out.println(obj1.myVar + "," + obj2.myVar);
+        int temp = obj1.myVar;
+        obj1.myVar = obj2.myVar;
+        obj2.myVar = temp;
+        System.out.println(obj1.myVar + "," + obj2.myVar);
     }
 
     public int calculateArea(int length, int breadth) {
