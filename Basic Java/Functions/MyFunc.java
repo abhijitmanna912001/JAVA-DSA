b2e8f7ebf4fcfce604a15a53391027ce68d7a33f
Abhijit Manna <79353253+abhijitmanna912001@users.noreply.github.com>
2023-12-26 14:50:05 +0530

    Swapping of two nos using Functions

--- Basic Java/Functions/MyFunc.java
+++ Basic Java/Functions/MyFunc.java
@@ -1,8 +1,21 @@
 public class MyFunc {
     public static void main(String[] args) {
         MyFunc mf = new MyFunc();
-        int res = mf.calculateArea(5, 2);
-        System.out.println(res);
+
+        int num1 = 30;
+        int num2 = 20;
+        mf.swap(num1, num2);
+        // System.out.println(num1 + "," + num2);
+        // int res = mf.calculateArea(5, 2);
+        // System.out.println(res);
+    }
+
+    public void swap(int val1, int val2) {
+        System.out.println(val1 + "," + val2);
+        int temp = val1;
+        val1 = val2;
+        val2 = temp;
+        System.out.println(val1 + "," + val2);
     }
 
     public int calculateArea(int length, int breadth) {
