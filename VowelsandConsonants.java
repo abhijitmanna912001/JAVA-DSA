f8d392c489df9971abce590f648d88311e7bb0e5
Abhijit Manna <79353253+abhijitmanna912001@users.noreply.github.com>
2023-12-26 11:35:29 +0530

    Now using For loop

--- VowelsandConsonants.java
+++ VowelsandConsonants.java
@@ -1,14 +1,15 @@
 public class VowelsandConsonants {
     public static void main(String[] args) {
         String name = "abhijit";
-        char ch = name.charAt(0);
+        int len = 7;
+        for (int pos = 0; pos < len; pos++) {
+            char ch = name.charAt(pos);
 
-        if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
-            System.out.println("Hi Vowel: " + ch);
-        }
+            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u')
+                System.out.println("Hi Vowel:\t" + ch);
 
-        else {
-            System.out.println("Hi Consonant: " + ch);
+            else
+                System.out.println("Hi Consonant:\t" + ch);
         }
     }
 }
