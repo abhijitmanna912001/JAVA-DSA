bebb155a85e781b0d878d9de577fa8fe98186573
Abhijit Manna <79353253+abhijitmanna912001@users.noreply.github.com>
2023-12-28 19:56:25 +0530

    Count Digits of a Number

---
+++ Maths/NumberSystem/CountDigits.java
@@ -0,0 +1,29 @@
+public class CountDigits {
+    public static void main(String[] args) {
+        countDigits(56743);
+        countDigitsLog(56743);
+    }
+
+    public static void countDigits(int num) {
+        if (num == 0) {
+            System.out.println("Digits are: 1");
+            return;
+        }
+        int counter = 0;
+        while (num > 0) {
+            num /= 10;
+            counter++;
+        }
+        System.out.println("Digits are: " + counter);
+    }
+
+    public static void countDigitsLog(int num) {
+        if (num == 0) {
+            System.out.println("Digits are: 1");
+            return;
+        }
+
+        int count = (int) ((int) Math.log(num) / Math.log(10)) + 1;
+        System.out.println("Digits are: " + count);
+    }
+}
