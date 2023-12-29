e899137c61b7047ef4a9e0ff37ec75252c967faf
Abhijit Manna <79353253+abhijitmanna912001@users.noreply.github.com>
2023-12-29 12:10:30 +0530

    Direct log10 method

--- Maths/NumberSystem/CountDigits.java
+++ Maths/NumberSystem/CountDigits.java
@@ -23,7 +23,8 @@ public class CountDigits {
             return;
         }
 
-        int count = (int) ((int) Math.log(num) / Math.log(10)) + 1;
+        // int count = (int) ((int) Math.log(num) / Math.log(10)) + 1;
+        int count = (int) Math.log10(num) + 1;
         System.out.println("Digits are: " + count);
     }
 }
