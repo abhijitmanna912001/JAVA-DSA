4c579b8c364a9fc34c4a56d2cd3a8b91749b1e51
Abhijit Manna <79353253+abhijitmanna912001@users.noreply.github.com>
2023-12-29 20:33:44 +0530

    Square Root by Newton Raphson Method

---
+++ Maths/NumberSystem/NewtonRaphson.java
@@ -0,0 +1,22 @@
+public class NewtonRaphson {
+    public static void main(String[] args) {
+        double result = squareRoot(400);
+        System.out.println("Square root: " + result);
+    }
+
+    public static double squareRoot(int num) {
+        double tol = 0.0001;
+        double root;
+        double x = num;
+
+        while (true) {
+            root = 0.5 * (x + (num / x));
+            double ans = x - root;
+            if (ans < tol) {
+                break;
+            }
+            x = root;
+        }
+        return root;
+    }
+}
