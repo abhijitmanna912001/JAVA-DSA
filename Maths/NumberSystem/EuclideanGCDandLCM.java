c637e8c2954df4c2f0783c7f91edd56622b356e2
Abhijit Manna <79353253+abhijitmanna912001@users.noreply.github.com>
2023-12-30 10:03:44 +0530

    GCD AND LCM using Euclidean Algorithm

--- Maths/NumberSystem/EuclideanGCDandLCM.java
+++ Maths/NumberSystem/EuclideanGCDandLCM.java
@@ -1,10 +1,11 @@
 // https://practice.geeksforgeeks.org/problems/gcd-of-two-numbers3459/1
 // I solved using these 2 logic also
 
-public class EuclideanGCD {
+public class EuclideanGCDandLCM {
     public static void main(String[] args) {
-        euclidianGCD(24, 36);
-        euclidianGCD2(24, 36);
+        // euclidianGCD(24, 36);
+        // euclidianGCD2(12, 24);
+        lcm(12, 24);
     }
 
     public static void euclidianGCD(int num1, int num2) {
@@ -24,7 +25,7 @@ public class EuclideanGCD {
         System.out.println("GCD is: " + res);
     }
 
-    public static void euclidianGCD2(int num1, int num2) {
+    public static int euclidianGCD2(int num1, int num2) {
         while (num1 != 0 && num2 != 0) {
             if (num1 > num2) {
                 num1 %= num2;
@@ -38,6 +39,12 @@ public class EuclideanGCD {
             res = num2;
 
         }
-        System.out.println("GCD is: " + res);
+        System.out.println(res);
+        return res;
+    }
+
+    public static void lcm(int num1, int num2) {
+        int res = (num1 * num2) / euclidianGCD2(num1, num2);
+        System.out.println("LCM is: " + res);
     }
 }
