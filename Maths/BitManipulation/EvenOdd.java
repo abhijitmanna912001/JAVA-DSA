9a162f9ade0b03d83a57d53e0156ce822b9be368
Abhijit Manna <79353253+abhijitmanna912001@users.noreply.github.com>
2024-01-01 12:30:31 +0530

    Gfg question which i solved linked the question

--- Maths/BitManipulation/EvenOdd.java
+++ Maths/BitManipulation/EvenOdd.java
@@ -1,3 +1,6 @@
+// https://www.geeksforgeeks.org/problems/check-whether-k-th-bit-is-set-or-not-1587115620/1?itm_source=geeksforgeeks&itm_medium=article&itm_campaign=bottom_sticky_on_article
+// I solved using the below logic
+
 public class EvenOdd {
     public static void main(String[] args) {
         printOddEven(44);
