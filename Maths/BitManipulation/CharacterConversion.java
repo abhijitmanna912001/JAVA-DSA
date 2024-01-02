22bcc765acfc88c1bee0f60b905836bd284b40c3
Abhijit Manna <79353253+abhijitmanna912001@users.noreply.github.com>
2024-01-02 20:22:44 +0530

    Conversion using ASCII values

--- Maths/BitManipulation/CharacterConversion.java
+++ Maths/BitManipulation/CharacterConversion.java
@@ -1,16 +1,27 @@
 public class CharacterConversion {
     public static void main(String[] args) {
-        char ch1 = convertCharToLowerCase('A');
-        System.out.println(ch1);
+        // char ch1 = convertCharToLowerCase('A');
+        // System.out.println(ch1);
         char ch2 = convertCharToUpperCase('a');
         System.out.println(ch2);
+        // printBits(~(1 << 5));
+        // printBits('_');
+    }
+
+    public static void printBits(int num) {
+        for (int i = 7; i >= 0; i--) {
+            System.out.print((num >> i) & 1);
+        }
+        System.out.println();
     }
 
     public static char convertCharToUpperCase(int num) {
-        return (char) (num & ~(1 << 5));
+        // return (char) (num & ~(1 << 5));
+        return (char) (num & '_');
     }
 
     public static char convertCharToLowerCase(int num) {
-        return (char) (num | (1 << 5));
+        // return (char) (num | (1 << 5));
+        return (char) (num | ' ');
     }
 }
