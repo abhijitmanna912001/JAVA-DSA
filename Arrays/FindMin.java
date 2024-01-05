b9f2d6e822e66b5a2d6bc67528937491c3375ed6
Abhijit Manna <79353253+abhijitmanna912001@users.noreply.github.com>
2024-01-05 20:43:24 +0530

    Find minimum element in an array

---
+++ Arrays/FindMin.java
@@ -0,0 +1,20 @@
+// https://www.geeksforgeeks.org/problems/find-minimum-and-maximum-element-in-an-array4428/1?utm_source=geeksforgeeks&utm_medium=ml_article_practice_tab&utm_campaign=article_practice_tab
+// I solved this question using both max and min logic
+
+public class FindMin {
+    public static void main(String[] args) {
+        int arr[] = { 2, 2, 3, 4, 1, 7, 2, 9 };
+        findMin(arr);
+    }
+
+    public static void findMin(int arr[]) {
+        // int min = arr[0];
+        int min = Integer.MAX_VALUE;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] < min) {
+                min = arr[i];
+            }
+        }
+        System.out.println("Minimum element is: " + min);
+    }
+}
