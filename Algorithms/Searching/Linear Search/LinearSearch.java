97243a4ac171c4e6c9f4c33707a822449431c29e
Abhijit Manna <79353253+abhijitmanna912001@users.noreply.github.com>
2024-01-05 12:09:41 +0530

    Searching of Multiple Occurences of a Number

--- Algorithms/Searching/Linear Search/LinearSearch.java
+++ Algorithms/Searching/Linear Search/LinearSearch.java
@@ -1,21 +1,43 @@
 public class LinearSearch {
     public static void main(String[] args) {
         int arr[] = { 3, 1, 3, 4, 5, 3 };
-        linearSearch(arr, 4);
+        // linearSearch(arr, 3, false);
+        // linearSearch(arr, 3, true);
+        linearSearchMultiple(arr, 3);
     }
 
-    public static void linearSearch(int arr[], int target) {
+    public static void linearSearch(int arr[], int target, boolean findLast) {
         int ans = -1;
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] == target) {
                 ans = i;
-                break;
+                if (findLast == false) {
+                    break;
+                }
             }
         }
         if (ans == -1) {
             System.out.println("Element is not present in array");
         } else {
             System.out.println("Found element at pos: " + ans);
+        }
+    }
+
+    public static void linearSearchMultiple(int arr[], int target) {
+        int ans[] = new int[arr.length];
+        int k = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == target) {
+                ans[k] = i;
+                k++;
+            }
+        }
+        if (k == 0) {
+            System.out.println("Element is not present in array");
+        } else {
+            for (int i = 0; i < k; i++) {
+                System.out.println("Found element at pos: " + ans[i]);
+            }
         }
     }
 }
