a6c182f4788d2336d3a4591f44069f63926c0c21
Abhijit Manna <79353253+abhijitmanna912001@users.noreply.github.com>
2024-01-05 21:32:36 +0530

    Learnt searching in 2D Array and solved 1 Gfg question and 2 Leetcode questions

--- Algorithms/Searching/Linear Search/LinearSearch.java
+++ Algorithms/Searching/Linear Search/LinearSearch.java
@@ -1,9 +1,22 @@
+// https://www.geeksforgeeks.org/problems/search-in-a-matrix17201720/1?utm_source=geeksforgeeks&utm_medium=ml_article_practice_tab&utm_campaign=article_practice_tab
+// https://leetcode.com/problems/search-a-2d-matrix/description/
+// https://leetcode.com/problems/search-a-2d-matrix-ii/description/
+// Solved these three questions using Linear Search in a 2D Array logic
+
 public class LinearSearch {
     public static void main(String[] args) {
         int arr[] = { 3, 1, 3, 4, 5, 3 };
         // linearSearch(arr, 3, false);
         // linearSearch(arr, 3, true);
-        linearSearchMultiple(arr, 3);
+        // linearSearchMultiple(arr, 3);
+        int arr2d[][] = {
+                { 2, 4, 1, 5, 1 },
+                { 6, 7, 3, 4, 2 },
+                { 8, 1, 2, 2, 3 },
+                { 5, 5, 5, 8, 4 }
+        };
+        linearSearch2DArray(arr2d, 4, false);
+
     }
 
     public static void linearSearch(int arr[], int target, boolean findLast) {
@@ -38,6 +51,27 @@ public class LinearSearch {
             for (int i = 0; i < k; i++) {
                 System.out.println("Found element at pos: " + ans[i]);
             }
+        }
+    }
+
+    public static void linearSearch2DArray(int arr[][], int target, boolean findLast) {
+        int outerIndex = -1;
+        int innerIndex = -1;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                if (target == arr[i][j]) {
+                    outerIndex = i;
+                    innerIndex = j;
+                    if (findLast == false) {
+                        break;
+                    }
+                }
+            }
+        }
+        if (outerIndex == -1) {
+            System.out.println("Element is not present in array");
+        } else {
+            System.out.println("Found element at pos: " + outerIndex + "," + innerIndex);
         }
     }
 }
