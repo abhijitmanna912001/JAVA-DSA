5616b42479f8762957af13850eb65bec5802c563
Abhijit Manna <79353253+abhijitmanna912001@users.noreply.github.com>
2024-01-06 22:43:08 +0530

    Binary Search Algorithm

---
+++ Algorithms/Searching/Binary Search/BinarySearch.java
@@ -0,0 +1,29 @@
+public class BinarySearch {
+    public static void main(String[] args) {
+        int arr[] = { 2, 5, 7, 11, 45, 87, 90 };
+        binarySearch(arr, 45);
+    }
+
+    static void binarySearch(int arr[], int target) {
+
+        int ans = -1;
+        int start = 0;
+        int end = arr.length - 1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (arr[mid] == target) {
+                ans = mid;
+                break;
+            } else if (arr[mid] < target) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+        if (ans == -1) {
+            System.out.println("Element not found");
+        } else {
+            System.out.println("Found element " + target + " at index: " + ans);
+        }
+    }
+}
