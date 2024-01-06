5854d61e68c19738a42631f8f305e0eeb722dc90
Abhijit Manna <79353253+abhijitmanna912001@users.noreply.github.com>
2024-01-06 11:48:45 +0530

    Maximum Sum in inner array of 2D Arrays

---
+++ Arrays/MaximumSumArray.java
@@ -0,0 +1,27 @@
+public class MaximumSumArray {
+    public static void main(String[] args) {
+        int arr2d[][] = {
+                { 2, 4, 1, 5, 1 },
+                { 6, 7, 3, 4, 2 },
+                { 8, 1, 2, 2, 3 },
+                { 5, 5, 5, 8, 4 }
+        };
+        findMaximumSumArray(arr2d);
+    }
+
+    public static void findMaximumSumArray(int arr[][]) {
+        int max = Integer.MIN_VALUE;
+        int resIndex = -1;
+        for (int i = 0; i < arr.length; i++) {
+            int sum = 0;
+            for (int j = 0; j < arr[i].length; j++) {
+                sum = sum + arr[i][j];
+            }
+            if (sum > max) {
+                max = sum;
+                resIndex = i;
+            }
+        }
+        System.out.println("Maximum subarray sum is: " + max + ", for the index " + resIndex);
+    }
+}
