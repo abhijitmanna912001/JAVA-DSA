0b54fe317d45dabb1e95e89ce9e87efae0e3e490
Abhijit Manna <79353253+abhijitmanna912001@users.noreply.github.com>
2024-01-07 14:10:10 +0530

    First and Last Occurence of a Number in Sorted Array

--- Algorithms/Searching/Binary Search/BinarySearch.java
+++ Algorithms/Searching/Binary Search/BinarySearch.java
@@ -3,12 +3,15 @@
 
 public class BinarySearch {
     public static void main(String[] args) {
-        int arr[] = { 2, 5, 7, 11, 45, 87, 90 };
-        int arr2[] = { 90, 87, 45, 11, 7, 5, 2 };
+        // int arr[] = { 2, 5, 7, 11, 45, 87, 90 };
+        // int arr2[] = { 90, 87, 45, 11, 7, 5, 2 };
+        int arr3[] = { 2, 2, 3, 4, 4, 4, 6, 8, 9 };
         // binarySearch(arr, 45);
         // binarySearchDesc(arr2, 45);
-        orderAgnosticbinarySearch(arr2, 45);
-        orderAgnosticbinarySearch(arr, 87);
+        // orderAgnosticbinarySearch(arr2, 45);
+        // orderAgnosticbinarySearch(arr, 87);
+        binarySearchFirstOccur(arr3, 4, true);
+        binarySearchFirstOccur(arr3, 4, false);
     }
 
     static void binarySearch(int arr[], int target) {
@@ -69,6 +72,32 @@ public class BinarySearch {
 
         else {
             binarySearchDesc(arr, target);
+        }
+    }
+
+    static void binarySearchFirstOccur(int arr[], int target, boolean isFirst) {
+        int ans = -1;
+        int start = 0;
+        int end = arr.length - 1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (arr[mid] == target) {
+                ans = mid;
+                if (isFirst) {
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
+                }
+            } else if (arr[mid] < target) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+        if (ans == -1) {
+            System.out.println("Element not found");
+        } else {
+            System.out.println("Found element " + target + " at index: " + ans);
         }
     }
 }
