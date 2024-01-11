2163cafb1636796cdcd5153ce38825237af8ba6c
Abhijit Manna <79353253+abhijitmanna912001@users.noreply.github.com>
2024-01-12 01:04:21 +0530

    Peak element in a sorted bitonic array

---
+++ Arrays/PeakElementBitonic.java
@@ -0,0 +1,34 @@
+// https://www.geeksforgeeks.org/problems/maximum-value-in-a-bitonic-array3001/1
+// Solved this question using below logic
+
+public class PeakElementBitonic {
+    public static void main(String[] args) {
+        int arr[] = { 3, 4, 2 };
+        int ans = findPeakElementBitonic(arr);
+        if (ans == -1) {
+            System.out.println("Element does not exist");
+        } else {
+            System.out.println("Found element " + arr[ans] + " at index: " + ans);
+        }
+    }
+
+    static int findPeakElementBitonic(int arr[]) {
+        int start = 0, end = arr.length - 1;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            int next = (mid + 1) % arr.length;
+            int prev = (mid - 1 + arr.length) % arr.length;
+
+            if (arr[mid] > arr[prev] && arr[mid] > arr[next]) {
+                return mid;
+            } else if (arr[mid] > arr[prev]) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+
+        return -1;
+    }
+}
