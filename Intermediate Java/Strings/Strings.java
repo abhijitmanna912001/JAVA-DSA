ffa1df3b84cdb87ac699d156b09233dd2c5ded08
Abhijit Manna <79353253+abhijitmanna912001@users.noreply.github.com>
2024-01-28 16:28:53 +0530

    Equal to Operator on Strings operation

--- Intermediate Java/Strings/Strings.java
+++ Intermediate Java/Strings/Strings.java
@@ -14,20 +14,41 @@ public class Strings {
         // System.out.println(brr[0]);
         // System.out.println(crr[0]);
 
-        int arr[] = new int[3];
-        arr[0] = 4;
-        String name1 = "Abhijit";
-        System.out.println(name1);
-        String name2 = name1;
-        int brr[] = arr;
-        brr[0] = 8;
-        name2 = "Amit";
-        System.out.println(name1);
-        int crr[] = new int[3];
-        crr[0] = 10;
-        String name3 = "Abhijit";
-        System.out.println(name1);
-        System.out.println(name2);
-        System.out.println(name3);
+        // int arr[] = new int[3];
+        // arr[0] = 4;
+        // String name1 = "Abhijit";
+        // System.out.println(name1);
+        // String name2 = name1;
+        // int brr[] = arr;
+        // brr[0] = 8;
+        // name2 = "Amit";
+        // System.out.println(name1);
+        // int crr[] = new int[3];
+        // crr[0] = 10;
+        // String name3 = "Abhijit";
+        // System.out.println(name1);
+        // System.out.println(name2);
+        // System.out.println(name3);
+
+        // String name1 = "Abhijit";
+        // String name2 = "Abhijit";
+        // String name3 = "Abhijit";
+        // String name4 = name1;
+        // System.out.println(name1 == name2);
+        // System.out.println(name2 == name3);
+        // System.out.println(name3 == name4);
+        // System.out.println(name4 == name2);
+        // System.out.println(name1 == name4);
+
+        int name1[] = new int[3];
+        int name2[] = new int[3];
+        int name3[] = new int[3];
+        int name4[] = name1;
+
+        System.out.println(name1 == name2);
+        System.out.println(name2 == name3);
+        System.out.println(name3 == name4);
+        System.out.println(name4 == name2);
+        System.out.println(name1 == name4);
     }
 }
