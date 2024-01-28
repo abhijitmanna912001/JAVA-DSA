c7cbd73168acdb280f9d99ed69fe4481bf322c1b
Abhijit Manna <79353253+abhijitmanna912001@users.noreply.github.com>
2024-01-28 18:09:24 +0530

    Usage of Equals Operator

--- Intermediate Java/Strings/Strings.java
+++ Intermediate Java/Strings/Strings.java
@@ -55,10 +55,15 @@ public class Strings {
         String name2 = new String("Shashwat");
         String name3 = new String("Shashwat");
         String name4 = name1;
-        System.out.println(name1 == name2);
-        System.out.println(name2 == name3);
-        System.out.println(name3 == name4);
-        System.out.println(name4 == name2);
-        System.out.println(name1 == name4);
+        System.out.println(name1.equals(name2));
+        System.out.println(name2.equals(name3));
+        System.out.println(name3.equals(name4));
+        System.out.println(name4.equals(name2));
+        System.out.println(name1.equals(name4));
+        // System.out.println(name1 == name2);
+        // System.out.println(name2 == name3);
+        // System.out.println(name3 == name4);
+        // System.out.println(name4 == name2);
+        // System.out.println(name1 == name4);
     }
 }
