0ac54976248a60495011fa8d518b8993bc1098ea
Abhijit Manna <79353253+abhijitmanna912001@users.noreply.github.com>
2024-02-13 23:06:46 +0530

    Multilevel Inheritance

--- Intermediate Java/OOPS/Inheritance.java
+++ Intermediate Java/OOPS/Inheritance.java
@@ -38,6 +38,54 @@
 // }
 
 // Multi Level Inheritance
+// class Terminator1 {
+//     String pname;
+//     int page;
+
+//     public void displayParent() {
+//         System.out.println("Parent name is " + pname);
+//     }
+// }
+
+// class Child1 extends Terminator1 {
+//     String c1name;
+//     int c1age;
+
+//     public void displayChild1() {
+//         System.out.println("Child1 name is " + c1name);
+//     }
+
+//     public void displayAge1() {
+//         System.out.println("Child1 age is " + c1age);
+//     }
+// }
+
+// class Child2 extends Child1 {
+//     String c2name;
+//     int c2age;
+
+//     public void displayChild2() {
+//         System.out.println("Child2 name is " + c2name);
+//     }
+// }
+
+// public class Inheritance {
+
+//     public static void main(String[] args) {
+//         Terminator1 t2 = new Terminator1();
+//         t2.pname = "Abhijit";
+//         t2.displayParent();
+//         Child1 c1 = new Child1();
+//         c1.pname = "Hello";
+//         c1.displayParent();
+//         c1.displayChild1();
+//         Child2 c2 = new Child2();
+//         c2.pname = "World";
+//         c2.displayParent();
+//     }
+// }
+
+// Hierarchical Inheritance
 class Terminator1 {
     String pname;
     int page;
@@ -46,7 +94,8 @@ class Terminator1 {
         System.out.println("Parent name is " + pname);
     }
 
-    // data hiding - restricting
+    // data hiding-restricting
+
     private void displayAge() {
         System.out.println("Parent age is " + page);
     }
@@ -73,6 +122,14 @@ class Child2 extends Child1 {
         System.out.println("Child2 name is " + c2name);
     }
 }
+class Child3 extends Child1 {
+    String c3name;
+    int c3age;
+
+    public void displayChild2() {
+        System.out.println("Child2 name is " + c3name);
+    }
+}
 
 public class Inheritance {
 
@@ -87,5 +144,6 @@ public class Inheritance {
         Child2 c2 = new Child2();
         c2.pname = "World";
         c2.displayParent();
+
     }
 }
