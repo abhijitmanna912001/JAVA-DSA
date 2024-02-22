c333b585d4fb9bf011d87cb79792b88eb6a9141d
Abhijit Manna <79353253+abhijitmanna912001@users.noreply.github.com>
2024-02-22 16:17:01 +0530

    Collections Framework

---
+++ Intermediate Java/OOPS/myCollection.java
@@ -0,0 +1,8 @@
+import java.util.*;
+
+public class myCollection {
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>();
+        System.out.println(list.size());
+    }
+}
