922b4f8b6d83f3ea6cb74bf598fc911eef3ccfb6
Abhijit Manna <79353253+abhijitmanna912001@users.noreply.github.com>
2024-02-27 16:46:49 +0530

    Dequeue using LinkedList

--- Intermediate Java/OOPS/Collection/myQueue.java
+++ Intermediate Java/OOPS/Collection/myQueue.java
@@ -2,15 +2,28 @@ import java.util.*;
 
 public class myQueue {
     public static void main(String[] args) {
-        Queue<Integer> queue = new ArrayDeque<>();
-        
+        // Deque<Integer> queue = new ArrayDeque<>();
+        Deque<Integer> queue = new LinkedList<>();
+
         queue.offer(5);
         queue.offer(7);
         queue.offer(1);
         queue.offer(8);
+
         System.out.println(queue);
-        System.out.println(queue.peek());
+        // System.out.println(queue.peek());
+        // System.out.println(queue.poll());
+        // System.out.println(queue);
+
+        System.out.println(queue.offerFirst(9));
+        System.out.println(queue.peekFirst());
+        System.out.println(queue.peekLast());
+        System.out.println(queue);
+        System.out.println(queue.offerLast(10));
+        System.out.println(queue);
         System.out.println(queue.poll());
-        System.out.println(queue);
+        System.out.println(queue.pollFirst());
+        System.out.println(queue.pollLast());
+        System.out.println(queue.poll());
     }
 }
