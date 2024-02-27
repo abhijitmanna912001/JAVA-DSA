8f7cbd0c09a010fc026a04ee3c7717c18f4deeff
Abhijit Manna <79353253+abhijitmanna912001@users.noreply.github.com>
2024-02-27 11:53:56 +0530

    Stack using List Interface

---
+++ Intermediate Java/OOPS/Collection/stackList.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class stackList {
+    public static void main(String[] args) {
+        Stack<Integer> stack = new Stack<>();
+        stack.push(1);
+        stack.push(5);
+        stack.push(1);
+        stack.push(3);
+        System.out.println(stack.peek());
+        System.out.println(stack);
+        System.out.println(stack.pop());
+        System.out.println(stack);
+        System.out.println(stack.size());
+
+    }
+}
