4f1c216bc0432708da8d9bdc001ef0e7d7568f39
Abhijit Manna <79353253+abhijitmanna912001@users.noreply.github.com>
2024-02-29 11:42:27 +0530

    PriorityQueue

--- Intermediate Java/OOPS/Collection/myQueue.java
+++ Intermediate Java/OOPS/Collection/myQueue.java
@@ -3,7 +3,10 @@ import java.util.*;
 public class myQueue {
     public static void main(String[] args) {
         // Deque<Integer> queue = new ArrayDeque<>();
-        Deque<Integer> queue = new LinkedList<>();
+        // Deque<Integer> queue = new LinkedList<>();
+        // Queue<Integer> queue = new PriorityQueue<>();
+        // Queue<Integer> queue = new PriorityQueue<>(Comparator.reverseOrder());
+        Queue<Integer> queue = new PriorityQueue<>(Collections.reverseOrder());
 
         queue.offer(5);
         queue.offer(7);
@@ -11,19 +14,22 @@ public class myQueue {
         queue.offer(8);
 
         System.out.println(queue);
+        System.out.println(queue.peek());
+        System.out.println(queue.poll());
+        System.out.println(queue);
         // System.out.println(queue.peek());
         // System.out.println(queue.poll());
         // System.out.println(queue);
 
-        System.out.println(queue.offerFirst(9));
-        System.out.println(queue.peekFirst());
-        System.out.println(queue.peekLast());
-        System.out.println(queue);
-        System.out.println(queue.offerLast(10));
-        System.out.println(queue);
-        System.out.println(queue.poll());
-        System.out.println(queue.pollFirst());
-        System.out.println(queue.pollLast());
-        System.out.println(queue.poll());
+        // System.out.println(queue.offerFirst(9));
+        // System.out.println(queue.peekFirst());
+        // System.out.println(queue.peekLast());
+        // System.out.println(queue);
+        // System.out.println(queue.offerLast(10));
+        // System.out.println(queue);
+        // System.out.println(queue.poll());
+        // System.out.println(queue.pollFirst());
+        // System.out.println(queue.pollLast());
+        // System.out.println(queue.poll());
     }
 }
