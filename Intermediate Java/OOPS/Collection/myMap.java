cb2ae1911724f6816c3eabb97cc74a6c7421dc52
Abhijit Manna <79353253+abhijitmanna912001@users.noreply.github.com>
2024-03-01 22:50:23 +0530

    Tree Map and its functions

--- Intermediate Java/OOPS/Collection/myMap.java
+++ Intermediate Java/OOPS/Collection/myMap.java
@@ -2,31 +2,37 @@ import java.util.*;
 
 public class myMap {
     public static void main(String[] args) {
-        Map<Integer, String> classMap = new LinkedHashMap<>();
+        Map<Integer, String> classMap = new TreeMap<>(Collections.reverseOrder());
         classMap.put(1, "Abhijit");
-        classMap.put(2, "Ayush");
+        classMap.put(7, "Ayush");
         System.out.println(classMap);
         classMap.putIfAbsent(3, "Param");
         System.out.println(classMap);
-        classMap.putIfAbsent(4, "Divya");
+        classMap.putIfAbsent(2, "Divya");
         System.out.println(classMap);
-        classMap.putIfAbsent(5, "Aayushi");
+        classMap.putIfAbsent(11, "Aayushi");
+
         System.out.println(classMap);
-        System.out.println(classMap.getOrDefault(4, "No one"));
-        System.out.println(classMap.containsKey(4));
-        System.out.println(classMap.containsValue("abhijit"));
 
-        for (Map.Entry<Integer, String> entry : classMap.entrySet()) {
-            System.out.print("Key: " + entry.getKey());
-            System.out.println(" Value: " + entry.getValue());
-        }
+        classMap.put(6, classMap.getOrDefault(6, "No One") + "Manna");
+        System.out.println(classMap);
 
-        // for (Integer key : classMap.keySet()) {
-        // System.out.println("Key: " + key);
+        // System.out.println(classMap.getOrDefault(4, "No one"));
+
+        // System.out.println(classMap.containsKey(4));
+        // System.out.println(classMap.containsValue("abhijit"));
+
+        // for (Map.Entry<Integer, String> entry : classMap.entrySet()) {
+        // System.out.print("Key: " + entry.getKey());
+        // System.out.println(" Value: " + entry.getValue());
         // }
 
-        for (String value : classMap.values()) {
-            System.out.println("Value: " + value);
-        }
+        // // for (Integer key : classMap.keySet()) {
+        // // System.out.println("Key: " + key);
+        // // }
+
+        // for (String value : classMap.values()) {
+        // System.out.println("Value: " + value);
+        // }
     }
 }
