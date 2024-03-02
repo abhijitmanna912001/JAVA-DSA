0818c3d69e4faa68ee798c35ccb8d378a0a67a29
Abhijit Manna <79353253+abhijitmanna912001@users.noreply.github.com>
2024-03-02 13:12:36 +0530

    TreeSet

--- Intermediate Java/OOPS/Collection/mySet.java
+++ Intermediate Java/OOPS/Collection/mySet.java
@@ -2,12 +2,12 @@ import java.util.*;
 
 public class mySet {
     public static void main(String[] args) {
-        Set<Integer> mySet = new LinkedHashSet<>();
+        Set<Integer> mySet = new TreeSet<>(Collections.reverseOrder());
         mySet.add(1);
-        mySet.add(2);
         mySet.add(3);
         mySet.add(4);
         mySet.add(5);
+        mySet.add(2);
         System.out.println(mySet);
         // System.out.println(mySet.contains(4));
         // System.out.println(mySet.remove(1));
