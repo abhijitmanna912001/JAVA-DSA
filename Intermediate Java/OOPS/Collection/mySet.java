be6bc5fb4aa125cb73d970fd70c490eb2c1a8a91
Abhijit Manna <79353253+abhijitmanna912001@users.noreply.github.com>
2024-03-02 12:48:03 +0530

    Working of HashSet internally

--- Intermediate Java/OOPS/Collection/mySet.java
+++ Intermediate Java/OOPS/Collection/mySet.java
@@ -5,13 +5,17 @@ public class mySet {
         Set<Integer> mySet = new HashSet<>();
         mySet.add(1);
         mySet.add(2);
-        mySet.add(1);
-        mySet.add(1);
-        mySet.add(1);
-        mySet.add(1);
+        mySet.add(3);
+        mySet.add(4);
+        mySet.add(5);
         System.out.println(mySet);
-        System.out.println(mySet.contains(4));
-        System.out.println(mySet.remove(1));
-        System.out.println(mySet);
+        // System.out.println(mySet.contains(4));
+        // System.out.println(mySet.remove(1));
+        // System.out.println(mySet);
+
+        Iterator<Integer> itr = mySet.iterator();
+        while (itr.hasNext()) {
+            System.out.println(itr.next());
+        }
     }
 }
